9fa144f928612f0296539b28015495a5354be575
rubal <https://acidaes@dev.azure.com/acidaes/DataNext/_git/DataNext>
2023-05-08 16:09:07 +0530

    Updating with new code

--- spark_dataframe/spark_dataframe_basics.py
+++ spark_dataframe/spark_dataframe_basics.py
@@ -48,3 +48,36 @@ print(type(df_selected_column_1))
 # Get a column from dataframe with dataframe type
 df_selected_column_2 = df_2.select('name')
 print(type(df_selected_column_2))
+
+# Get a row from dataframe
+print(df_2.head(1)[0])
+
+# Create new column
+df_2.withColumn("double_age", df_2['age'] * 2).show()
+
+# Rename a column
+df_2.withColumnRenamed("age", "New_Age").show()
+
+# Register this dataframe
+df_1.createOrReplaceTempView("people")
+
+# Call the above dataframe using SQL
+result = spark.sql('SELECT * FROM people WHERE age = 19')
+result.show()
+
+# Read a JSON file into a DataFrame
+df_3 = spark.read.csv("../data_source/appl_stock.csv", inferSchema=True, header=True)
+df_3.printSchema()
+df_3.show()
+
+# Use filter
+df_3.filter("Close < 500").select(["Date", "Open"]).show()
+
+# Multiple filters
+df_3.filter((df_3["Close"] < 500) & (df_3["Open"] > 200)).show()
+
+# Rows as dictionary
+result_in_rows = df_3.filter((df_3["Close"] < 500) & (df_3["Open"] > 200)).collect()
+row = result_in_rows[0]
+print(row)
+print(row.asDict()['Volume'])
